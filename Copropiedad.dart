8fdea4d7b1b37462a11d2dc3a68d9f0c00205117
Chimbi <afrestrepocastro@gmail.com>
2018-09-02 12:31:01 -0500

    Include inheritance concepts
    Create person and the owner of the apartment that receive the attributes and methods of person

--- Copropiedad.dart
+++ Copropiedad.dart
@@ -9,24 +9,42 @@ class propertyManager{
   String _name;
   int fee;
   var properties; //How do you define list explicitly?
-  
+
   propertyManager(this._name, this.fee, this.properties);
   
   String visitProperty(building property) => "The property manager visit the property ${property.name}"; 
   
   String get getName => _name; //getter
-//   void setName(String value) => this.name = value; //Setter as a function
   set setName(String value) => _name = value; //setter
-  
-  
 }
+
+class person{
+	String name, lastName;
+  bool haveKids;
+  bool havePets;
+  
+  void showName() => print(this.name);
+  
+}  
+
+class owner extends person{
+  int apartmentNumber;
+  int apartmentSize;
+  
+  void showSize() => print(this.apartmentSize);
+}
+  
+
+  //   delegatedManager extends propertyManager{}
 
 void main() {
   var fifthAvenue = new building("FifthAvenue building",300, 35);
   var prop2 = new building("Flatiron", 200, 20);
   var james = new propertyManager("James",2000,[fifthAvenue,prop2]);
   
-//   james.setName("James Bond"); //set name as a function
+  var apt701 = new owner();  
+  apt701.name ="Andres";
+  apt701.showName();  //show how owner inherits the person methods
   
   james.setName = "James Bond";//   james.name="Pepe";
   
